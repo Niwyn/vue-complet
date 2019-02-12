7a4867c9b54313a9b0fb905f8b02ee277d51c449
Niwyn <theo.bremaud@gmail.com>
2019-02-12 15:22:24 +0100

    mid

---
+++ src/components/Post.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div v-html="postTab.title.rendered" class="h4 title"></div>
+    <div class="font-italic mb-2">Publié le {{formatDate(postTab.date)}}, il y a {{calcDate(postTab.date)}}</div>
+    <div v-html="postTab.content.rendered.slice(0, 200)"></div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import moment from 'moment'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
+export default {
+  name: 'Post',
+  props: ['postTab'],
+  methods: {
+    calcDate: function (date) {
+      return moment(date, 'YYYYMMDD').fromNow()
+    },
+    formatDate: function (date) {
+      return moment(date).format('h:mm, DD/MM/YYYY')
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+  .title{
+    color: #DAA588;
+    font-weight: bold;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div v-if="posts && posts.length" class="row">
+      <div v-for="post of posts" class="col-lg-6 post">
+        <Post :postTab="post"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Post from '@/components/Post.vue'
+import axios from 'axios'
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
+
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Post
+  },
+  data () {
+    return {
+      posts: []
+    }
+  },
+  created () {
+    axios.get(`http://www.madeinblue.com/wp-json/wp/v2/posts`)
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.posts = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+    axios.get(`http://www.madeinblue.com/wp-json/wp/v2/posts`)
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.posts = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
   }
 }
 </script>
+
+<style>
+  .container{
+    padding-top: 100px;
+  }
+</style>
